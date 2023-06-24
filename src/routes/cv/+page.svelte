80d238c482eead9e154ef363e12762fdeb2e17a8
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-24 14:07:53 -0700

    add cv

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Link from '$lib/components/Link.svelte';
+	import cv from '$lib/assets/Resume_CV.pdf';
+</script>
+
+<object
+	class="w-full h-[550px]"
+	data={cv}
+	type="application/pdf"
+	title="Resume/CV"
+	aria-label="Resume/CV"
+>
+	<p>
+		Unable to display PDF file.{' '}
+		<Link href={cv}>Download</Link>{' '}
+		instead.
+	</p>
+</object>
