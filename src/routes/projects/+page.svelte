2f2a78e14d2df4ff597980ef368400d42d40f388
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-24 15:58:20 -0700

    add repo list

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Repo from '$lib/components/Repo.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+{#each data.repos as repo}
+	<Repo url={repo.html_url} name={repo.name} description={repo.description} />
+{/each}
