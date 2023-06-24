e6aa354ecad16314437c192dcd871f489227038b
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-24 13:32:30 -0700

    add nav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let links: Record<string, string>;
+</script>
+
+<nav class="flex justify-center">
+	{#each Object.entries(links) as [name, href]}
+		<a
+			class="block text-2xl underline text-orange-500 m-2 hover:bg-orange-500/30 hover:no-underline p-1 rounded transition-all"
+			{href}
+		>
+			{name}
+		</a>
+	{/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import '../app.css';
 	import Banner from '$lib/components/Banner.svelte';
+	import Nav from '$lib/components/Nav.svelte';
+
+	const LINKS = {
+		Home: '/',
+		Projects: '/projects',
+		CV: '/cv'
+	};
 </script>
 
 <Banner />
+<Nav links={LINKS} />
 <main class="mx-auto px-4 md:px-8 max-w-4xl">
 	<slot />
 </main>
