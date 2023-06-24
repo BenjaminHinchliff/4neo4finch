2974798feee4f6ec396610dd07423ef738d8f301
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-24 01:07:18 -0700

    add tailwind and banner

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.svg';
+</script>
+
+<div class="relative py-20 rounded bg-orange-500 shadow overflow-hidden">
+	<div class="absolute left-0 right-0 top-0 bottom-0 z-0 opacity-30">
+		<img class="mx-auto h-full" src={logo} alt="logo" />
+	</div>
+	<h1 class="relative text-6xl text-center font-light">Benjamin Hinchliff</h1>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+	import Banner from '$lib/components/Banner.svelte';
+</script>
+
+<Banner />
+<main class="mx-auto px-4 md:px-8 max-w-4xl">
+	<slot />
+</main>
+
+<style lang="postcss">
+	:global(html) {
+		@apply bg-gray-100;
+		@apply dark:bg-gray-900;
+		@apply dark:text-white;
+	}
+</style>
