18f2b18e73b8730918f759d9428b472278e57ed1
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-26 10:35:06 -0700

    add dark mode toggle

---
+++ src/lib/components/DarkToggle.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Moon, Sun } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+
+	let dark: boolean | undefined = undefined;
+
+	const isDark = () =>
+		localStorage.theme === 'dark' ||
+		(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+	onMount(() => {
+		dark = isDark();
+	});
+
+	const toggleDark = () => {
+		dark = !dark;
+		if (dark) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+		localStorage.setItem('dark', dark ? 'dark' : 'light');
+	};
+</script>
+
+{#if dark !== undefined}
+	<button class="absolute top-2 right-2" on:click={toggleDark}>
+		{#if dark}
+			<Moon />
+		{:else}
+			<Sun />
+		{/if}
+	</button>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import Banner from '$lib/components/Banner.svelte';
 	import Nav from '$lib/components/Nav.svelte';
+	import DarkToggle from '$lib/components/DarkToggle.svelte';
 
 	const LINKS = {
 		Home: '/',
@@ -10,8 +11,13 @@
 	};
 </script>
 
+<svelte:head>
+	<script type="text/javascript" src="/dark-mode.min.js"></script>
+</svelte:head>
+
 <Banner />
 <Nav links={LINKS} />
+<DarkToggle />
 <main class="mx-auto px-4 md:px-8 max-w-4xl">
 	<slot />
 </main>
@@ -19,7 +25,8 @@
 <style lang="postcss">
 	:global(html) {
 		@apply bg-gray-100;
-		@apply dark:bg-gray-900;
-		@apply dark:text-white;
+	}
+	:global(:is(.dark)) {
+		@apply bg-gray-900 text-white;
 	}
 </style>
