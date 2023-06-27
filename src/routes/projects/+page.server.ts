573eeb534988d39314544a1676de752eae60bd15
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-06-27 13:37:36 -0700

    add log for octokit connection

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -4,6 +4,9 @@ import type { PageServerLoad } from './$types';
 import { getPage } from '$lib/utils/get-page';
 
 const octokit = new Octokit({ auth: GITHUB_API_TOKEN });
+octokit.rest.users
+	.getAuthenticated()
+	.then((user) => console.log(`Sucessfully created octokit instance: ${user.data.name}`));
 
 export const load: PageServerLoad = async ({ url }) => {
 	const PER_PAGE = 20;
