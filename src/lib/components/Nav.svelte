691103818fd123b565359f6a5bbd6f37070c5b6d
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-07-08 13:42:07 -0700

    fix link page visibility

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Link from '$lib/components/Link.svelte';
+	import { cn } from '$lib/utils/cn';
 
 	export let links: Record<string, string>;
+
+	let pathname: string | undefined;
+	page.subscribe((page) => {
+		pathname = page.url.pathname;
+	});
 </script>
 
 <nav class="flex justify-center">
 	{#each Object.entries(links) as [name, href]}
-		<Link class="block text-2xl m-2 p-1" {href}>
+		<Link class={cn('block text-2xl m-2 p-1', pathname === href && 'no-underline')} {href}>
 			{name}
 		</Link>
 	{/each}
