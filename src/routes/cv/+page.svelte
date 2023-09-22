a5b1ea4b3be792950b42dd7a2bd05a2c581e2c13
Ben Hinchliff <46175830+BenjaminHinchliff@users.noreply.github.com>
2023-09-22 00:23:36 -0700

    Update cv path

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import Link from '$lib/components/Link.svelte';
-	import cv from '$lib/assets/Resume_CV.pdf';
+	
+	const CV = "https://raw.githubusercontent.com/BenjaminHinchliff/resume/main/resume.pdf"
 </script>
 
 <object
 	class="w-full h-[550px]"
-	data={cv}
+	data={CV}
 	type="application/pdf"
 	title="Resume/CV"
 	aria-label="Resume/CV"
 >
 	<p>
 		Unable to display PDF file.{' '}
-		<Link href={cv}>Download</Link>{' '}
+		<Link href={CV}>Download</Link>{' '}
 		instead.
 	</p>
 </object>
